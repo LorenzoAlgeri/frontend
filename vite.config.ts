f9903bddcaf80c71a0374f4e902b794ac439af82
LorenzoAlgeri <148132548+LorenzoAlgeri@users.noreply.github.com>
2025-07-15 23:33:33 +0000

    fix: vite + struttura finalizzata

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
-export default defineConfig({
+export default defineConfig(async () => ({
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -35,4 +35,4 @@ export default defineConfig({
       deny: ["**/.*"],
     },
   },
-});
+}));
